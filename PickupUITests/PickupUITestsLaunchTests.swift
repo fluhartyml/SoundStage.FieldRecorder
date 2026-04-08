8c899477aa47bdcfcc7c2ab63a5587d78c0bb078
Michael Fluharty <michael@fluharty.com>
2026-04-08 12:13:48 -0500

    Initial Commit

---
+++ PickupUITests/PickupUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  PickupUITestsLaunchTests.swift
+//  PickupUITests
+//
+//  Created by Michael Fluharty on 4/8/26.
+//
+
+import XCTest
+
+final class PickupUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
